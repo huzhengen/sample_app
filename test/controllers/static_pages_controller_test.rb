a217bfa7f596e586e222a4dcca2a2281bbef91fd
hukai <huzhengen@163.com>
2021-01-27 17:22:54 +0800

    helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,7 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_titl}"
   end
 
   test "should get help" do
