94d0b6366abb0558f1593e033c298fc212dff4ab
hukai <huzhengen@163.com>
2021-02-04 17:17:17 +0800

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,25 @@ class UsersController < ApplicationController
   attr_accessor :name, :email
 
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 处理注册成功的情况
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
